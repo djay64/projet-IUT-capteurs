fdf83bfc8ec3acbf4202c88899203ec48dc133b1
Jerome Doyenard <jdoyenard001@iparla.iutbayonne.univ-pau.fr>
2020-02-06 22:18:02 +0100

    création entité capteur

---
+++ src/Migrations/Version20200206210634.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206210634 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE capteur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(10) NOT NULL, latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE TABLE1');
+        $this->addSql('DROP TABLE TABLE2');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE TABLE1 (nom INT DEFAULT NULL, latitude NUMERIC(5, 3) DEFAULT NULL, longitude NUMERIC(5, 3) NOT NULL, DateTime DATETIME DEFAULT NULL, P1 INT NOT NULL, P2 INT NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE TABLE2 (sensor_id INT DEFAULT NULL, sensor_type VARCHAR(6) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`, location INT DEFAULT NULL, lat NUMERIC(5, 3) DEFAULT NULL, lon NUMERIC(5, 3) DEFAULT NULL, timestamp DATETIME DEFAULT NULL, P1 NUMERIC(5, 2) DEFAULT NULL, durP1 VARCHAR(10) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`, ratioP1 VARCHAR(10) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`, P2 NUMERIC(4, 2) DEFAULT NULL, durP2 VARCHAR(10) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`, ratioP2 VARCHAR(10) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE capteur');
+    }
+}
