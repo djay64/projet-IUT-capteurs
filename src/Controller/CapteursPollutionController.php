9b056fba888c63013b46ddfcc2acdeb73ca8caa4
Remy Lartiguelongue <rlartiguelon@iutbayonne.univ-pau.fr>
2020-02-26 13:20:19 +0100

    Mise en place du formulaire de generation de graphique

--- src/Controller/CapteursPollutionController.php
+++ src/Controller/CapteursPollutionController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CapteursPollutionController extends AbstractController
 {
@@ -22,6 +26,24 @@ class CapteursPollutionController extends AbstractController
     public function accueil()
     {
         return $this->render('capteurs_pollution/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/genererGraphique", name="capteurs_pollution_genererGraphique")
+     */
+    public function genererGraphique(Request $requete){
+        $saisieFiltres = ['titre' => 'Titre du graphique'];
+
+        $formulaireFiltres = $this->createFormBuilder($saisieFiltres)
+        ->add('titre', TextType::class)
+        ->add('dateDebut', DateType::class, ['widget' => 'single_text'])
+        ->add('dateFin', DateType::class, ['widget' => 'single_text'])
+        ->add('typeParticule', ChoiceType::class, ['choices' => ['PM 2.5' => 'pm25', 'PM 10' => 'pm10'], 'expanded' => true, 'multiple' => true])
+        ->add('capteurs', ChoiceType::class, ['choices' => ['Capteur 1' => 'capteur1', 'Capteur 2' => 'capteur2', 'Capteur 3' => 'capteur3'], 'expanded' => false, 'multiple' => true])
+        ->add('typeGraphique', ChoiceType::class, ['choices' => ['Barre' => 'barre', 'Baton' => 'baton', 'Histogramme' => 'histogramme', 'Radar' => 'radar', 'Nuage de points' => 'nuage', 'Secteur' => 'secteur', 'Courbe' => 'courbe', 'Aires' => 'aire'], 'expanded' => true, 'multiple' => false])
+        ->getForm();
+
+        return $this->render('capteurs_pollution/genererGraphique.html.twig', ['selectionFiltres' => $formulaireFiltres->createView()]);
     }
 
 }
